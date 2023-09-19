901870f2c218f758339d459dc06e36a157027592
hanhdt268 <nguyenvanhanh686820@gmail.com>
2023-09-19 20:30:23 +0700

    first commit

---
+++ src/main/java/com/example/graduationbe/GraduationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graduationbe/GraduationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduationbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
